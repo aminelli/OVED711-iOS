2b8f425c0f4590d5d3e5e9f40bcf9902aafb8f06
aminelli <antonio.minelli@etlforma.com>
2026-06-05 15:06:42 +0200

    upd

---
+++ Esercizi/ARCDemo/ARCDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test002App.swift
+//  Test002
+//
+//  Created by Antonio Minelli on 04/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct Test002App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
